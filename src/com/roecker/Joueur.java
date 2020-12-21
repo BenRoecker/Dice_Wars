dd4241b2702834af7ec37ad8c021766704b764ab
Benjamin ROECKER <benjamin.roecker@efrei.net>
2020-12-21 10:44:43 +0100

    ajout des territoires au hasard parmi les joueurs et ajout de l'affichage des territoires de chaque joueurs

--- src/com/roecker/Joueur.java
+++ src/com/roecker/Joueur.java
@@ -7,17 +7,24 @@ public class Joueur {
     ArrayList<Territoire> ListeTerritoire;
     public Joueur(int id) {
         this.id = id;
+        this.ListeTerritoire = new ArrayList<>();
     }
     public int getId(){
         return this.id;
     }
-
+    public ArrayList<Territoire> getListeTerritoire(){
+        return this.ListeTerritoire;
+    }
     public void addTerritoire(Territoire add){
         ListeTerritoire.add(add);
     }
 
     @Override
     public String toString() {
-        return "Joueur n°"+this.getId();
+        StringBuilder rendu = new StringBuilder("Joueur n°"+this.getId()+", territoire : ");
+        for(Territoire i : ListeTerritoire){
+            rendu.append(i.toString());
+        }
+        return String.valueOf(rendu);
     }
 }
--- src/com/roecker/Partie.java
+++ src/com/roecker/Partie.java
@@ -1,18 +1,35 @@
 package com.roecker;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 
 public class Partie {
     private final int NBjoueurs;
+
+    //Inisialisation
     Partie(int joueurs){
         this.NBjoueurs = joueurs;
         Carte map  = new Carte(this.NBjoueurs);
+        ArrayList<Territoire> territoires = new ArrayList<>();
+        Territoire[][] tableau = map.getMap();
+        for (Territoire[] value : tableau) {
+            territoires.addAll(Arrays.asList(value));
+        }
+        Random rand = new Random();
         ArrayList<Joueur> challenger = new ArrayList<>();
         for(int i = 0;i < NBjoueurs; i ++){
             Joueur J = new Joueur(i);
+            for( int j = 0; j < 4; j ++){
+                int randomIndex = rand.nextInt(territoires.size());
+                Territoire randomTerritoire = territoires.get(randomIndex);
+                territoires.remove(randomIndex);
+                J.addTerritoire(randomTerritoire);
+            }
             challenger.add(J);
         }
         System.out.println(map);
         System.out.println(challenger);
+
     }
 }
