0a3f809cbbf43529825876bac0e4d68631af7ee6
Benjamin ROECKER <benjamin.roecker@efrei.net>
2020-12-30 10:23:43 +0100

    sécurisation de la demande de joueurs

--- src/com/roecker/Main.java
+++ src/com/roecker/Main.java
@@ -6,7 +6,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner saisieJoueurs = new Scanner(System.in);
         System.out.println("Veuillez saisir le nombre de joueurs :");
-        int ent = saisieJoueurs.nextInt();
+        int ent;
+        do{
+            System.out.println("Un nombre compris entre 1 et 10");
+            ent = saisieJoueurs.nextInt();
+        }while(ent <= 1 || ent >= 11);
 	    Partie jeu = new Partie(ent);
     }
 }
