5a535144e72d4f84c5ec59ee35c4c849c2b5e759
Benjamin ROECKER <benjamin.roecker@efrei.net>
2020-12-30 11:49:04 +0100

    sécurisation de la demande de joueurs

--- src/com/roecker/Main.java
+++ src/com/roecker/Main.java
@@ -1,16 +1,22 @@
 package com.roecker;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner saisieJoueurs = new Scanner(System.in);
         System.out.println("Veuillez saisir le nombre de joueurs :");
-        int ent;
+        int ent = 0;
+        boolean type = true;
         do{
+            Scanner saisieJoueurs = new Scanner(System.in);
             System.out.println("Un nombre compris entre 1 et 10");
-            ent = saisieJoueurs.nextInt();
-        }while(ent <= 1 || ent >= 11);
+            try{
+                ent = saisieJoueurs.nextInt();
+            }catch(InputMismatchException e){
+                type = false;
+            }
+        }while(ent <= 1 || ent >= 11 || type);
 	    Partie jeu = new Partie(ent);
     }
 }
--- src/com/roecker/Partie.java
+++ src/com/roecker/Partie.java
@@ -3,6 +3,7 @@ package com.roecker;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Partie {
     private final int NBjoueurs;
@@ -40,7 +41,21 @@ public class Partie {
             }
             challenger.add(J);
         }
+        int lol = attackJoueur();
+        System.out.println(lol);
         System.out.println(map);
         System.out.println(challenger);
+    }
+
+    public int attackJoueur(){
+        Scanner saisieIDs = new Scanner(System.in);
+        System.out.println("Veuillez saisir le teritoire qui attaque suivi du territoire à attaquer :");
+        int ent, ent1;
+        do{
+            System.out.println("Un nombre compris entre 1 et "+NBjoueurs*4);
+            ent = saisieIDs.nextInt();
+            ent1 = saisieIDs.nextInt();
+        }while(ent < 0 || ent >= NBjoueurs*4 || ent1 < 0 || ent1 >= NBjoueurs);
+        return ent1;
     }
 }
