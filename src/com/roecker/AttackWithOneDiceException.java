564376687e29723595c9a602c394d373840afa6e
Benjamin ROECKER <44980772+BenRoecker@users.noreply.github.com>
2021-01-12 21:51:50 +0100

    Add files via upload

--- src/com/roecker/AttackWithOneDiceException.java
+++ src/com/roecker/AttackWithOneDiceException.java
@@ -2,9 +2,6 @@ package com.roecker;
 
 public class AttackWithOneDiceException extends Exception{
     public AttackWithOneDiceException(){
-    }
-    public AttackWithOneDiceException(String message)
-    {
-        super(message);
+        super("\u001B[31m"+"Tu ne peux pas attaquer avec un territoire qui contient un seul dés."+"\u001B[0m");
     }
 }
